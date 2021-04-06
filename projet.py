a384b3a60bda3b6b34d21ecffba3897dea7fb855
Hady31-upmc <79153184+Hady31-upmc@users.noreply.github.com>
2021-04-06 17:39:45 +0200

    Update projet.py

--- projet.py
+++ projet.py
@@ -5,22 +5,31 @@ import math
 import numpy as np
 import time
 
-Passe = True
+
+Pause = True 
 Arret = True 
 angle = random.uniform(0,2*math.pi) 
-Rayon = 150
-c = 0  
-tps = 50
+Rayon = 80
+c = 0 
+s = 15
+Largeur = 700
+Hauteur = 700
+X0 = Largeur/2
+Y0 = Hauteur/2   
+TEMP = 55538
 
 def masseG() : 
-    global Rayon
-    Rayon = 200
-    tps = 60
+    global Rayon,s
+    if Arret == True : 
+        Rayon = 100
+        s = 20
+    
 def masseP() : 
-    global Rayon
-    Rayon = 120
-    tps = 45
-
+    global Rayon,s
+    if Arret == True : 
+        Rayon = 60 
+        s = 10  
+    
 class StopWatch(Frame):  
     """ Implémentation du chronomètre """                                                                
     def __init__(self, parent=None, **kw):        
@@ -51,9 +60,7 @@ class StopWatch(Frame):
         hseconds = int((elap - minutes*60.0 - seconds)*100)                
         self.timestr.set('%02d:%02d:%02d' % (minutes, seconds, hseconds))
         c+=1
-        if (seconds == tps): #Choisi quand l'étoile s'arrète de se former . 
-            Passe = True  
-        elif(minutes == 2) : 
+        if (seconds == s): #Choisi quand l'étoile s'arrète de se former . 
             Arret = True
             
             
@@ -79,28 +86,45 @@ class StopWatch(Frame):
             self._running = 1
 def Cercle():
     """ Dessine un cercle de centre (x,y) et de rayon r """
-    global Arret,Passe
+    global Arret,Pause,ovale,bloque
     t = random.uniform(0,2*math.pi)
     R = random.uniform(0,Rayon)
     x = R*math.cos(t) + Largeur/2
     y = R*math.sin(t) + Hauteur/2 
-    if Arret == False and Passe == False :
+    if Arret == False and Pause == False :
         r = 2
         oval = Canevas.create_oval(x-r, y-r, x+r, y+r, outline='white', fill='white')
-        # appel de la fonction Cercle() après une pause de 1 millisecondes
+        #appel de la fonction Cercle() après une pause de 1 millisecondes
         Mafenetre.after(1,Cercle)
-    elif Arret == False and Passe == True :  
-        r = 8 
-        oval = Canevas.create_oval(x-r, y-r, x+r, y+r, outline='light blue', fill='light blue')
-        # appel de la fonction Cercle() après une pause de 20 millisecondes
-        Mafenetre.after(20,Cercle)
+    elif Arret == True and Pause == False: 
+        ovale = Canevas.create_oval(X0-Rayon-2,Y0-Rayon-2,X0+Rayon+2,Y0+Rayon+2,outline = "white",fill="white")
+        Temp() 
+        Mafenetre.after(1,Cercle)
 
+def Temp() :  
+    global ovale,Pause
+    sw.Stop()
+    Pause = True 
+    #if Arret == True: 
+    if float(temp.get()) >= 1649 and float(temp.get()) <= 3316 : 
+        Canevas.itemconfigure(ovale,outline="red",fill = "red")
+    elif float(temp.get()) > 3316 and float(temp.get()) <= 4704:
+        Canevas.itemconfigure(ovale,outline="orange",fill = "orange")
+    elif float(temp.get()) > 4704 and float(temp.get()) <= 5816 : 
+        Canevas.itemconfigure(ovale,outline="light yellow",fill = "light yellow")
+    elif float(temp.get()) > 5816 and float(temp.get()) <= 9704 : 
+        Canevas.itemconfigure(ovale,outline="white",fill = "white")
+    elif float(temp.get()) > 9704 and float(temp.get()) <= 27760: 
+        Canevas.itemconfigure(ovale,outline="light blue",fill = "light blue")
+    else : 
+        Canevas.itemconfigure(ovale,outline="blue",fill = "Blue")
+        
 def pied() : 
-    global Arret,Passe
+    global Arret,Pause
     L=[] 
-    for i in range(0,8) : 
-        L.append(i*math.pi/4 + angle)
-    d = random.randint(0,7)
+    for i in range(0,4) : 
+        L.append(i*math.pi/2 + angle)
+    d = random.randint(0,3)
     x0 = Rayon*math.cos(L[d]) + Largeur/2
     y0 = Rayon*math.sin(L[d]) + Hauteur/2 
     t = random.uniform(0,math.pi)
@@ -110,36 +134,39 @@ def pied() :
     y = y0 + m + R*math.sin(L[d])*math.tan(t) 
     r = 2
     Canevas.create_oval(x-r, y-r, x+r, y+r, outline='white', fill='white')
-    if Arret == False and Passe == False:
+    if Arret == False and Pause == False:
         Mafenetre.after(1,pied)
         
 def Pause():
     """" Met en pause l'animation"""
-    global Arret
+    global Pause
     sw.Stop()
-    Arret = True
+    Pause = True
 
 def Demarrer(): 
     """ Démarre l'animation """ 
-    global Arret,Passe,c
+    global Arret,Pause,c
     Canevas.delete(ALL)
     if (c>1):
         sw.Reset()
         c = 0 
     sw.Start()
+    Pause = False
     Arret = False
-    Passe = False
     Cercle() # un seul appel à cette fonction
     pied() 
+    
         
 def Reprendre():
     """" Permet de reprendre l'animation"""
-    global Arret 
+    global Pause
     sw.Start()
-    Arret = False
+    Pause = False
     Cercle()
     pied()
+
     
+#Temp()
 # Création de la fenêtre principale (main window)
 Mafenetre = Tk()
 Mafenetre.title('Animation')
@@ -148,29 +175,36 @@ Mafenetre.title('Animation')
 Largeur = 700
 Hauteur = 700
 Canevas = Canvas(Mafenetre, width = Largeur, height =Hauteur, bg ='black')
-Canevas.pack(padx =5, pady =5)
+Canevas.pack(side = LEFT, padx =5, pady =5)
 sw = StopWatch(Mafenetre)
-sw.pack(side = TOP)
+sw.pack(side = TOP) 
 
-#création des boutons 
-Boutonmasse1 = Button(Mafenetre,text="Masse > 1,989 × 10^30 kg", command = masseG)
-Boutonmasse1.pack(side = RIGHT,padx=10,pady=10) 
 
-Boutonmasse2 = Button(Mafenetre,text="Masse < 1,989 × 10^30 kg", command = masseP)
-Boutonmasse2.pack(side = RIGHT,padx=10,pady=10)
+#création des boutons de Masse
+Boutonmasse1 = Button(Mafenetre,text="Masse > 1,989 × 10^30 kg", command = masseG,activebackground= "light blue",bg = "light blue")
+Boutonmasse1.pack(padx=5,pady=5) 
+
+Boutonmasse2 = Button(Mafenetre,text="Masse < 1,989 × 10^30 kg", command = masseP,activebackground= "light blue",bg = "light blue")
+Boutonmasse2.pack(padx=10,pady=10)
 # Création d'un widget Button (bouton Démarrer)
-BoutonGo = Button(Mafenetre, text ='Démarrer', command = Demarrer)
-BoutonGo.pack(side = LEFT, padx = 10, pady = 10)
+BoutonGo = Button(Mafenetre, text ='Démarrer', command = Demarrer,activebackground= "light green",bg = "light green")
+BoutonGo.pack(padx = 10, pady = 15)
 
 # Création d'un widget Button (bouton Pause)
-BoutonArreter = Button(Mafenetre, text ='Pause', command = Pause)
-BoutonArreter.pack(side = LEFT, padx = 5, pady = 5)
+BoutonArreter = Button(Mafenetre, text ='Pause', command = Pause,activebackground= "red",bg = "red")
+BoutonArreter.pack(padx = 5, pady = 20)
 
 # Création d'un widget Button (bouton Reprendre)
-Button(Mafenetre, text='Reprendre', command=Reprendre).pack(side=LEFT)
+Button(Mafenetre, text='Reprendre', command=Reprendre,activebackground= "light yellow",bg = "light yellow").pack(padx = 15,pady= 20)
+
+# Création d'un widget température
+temp = tk.StringVar()
+temp.set(5000)
+widget_temp = tk.Scale(Mafenetre,from_=1649,to=TEMP,resolution=1,orient=tk.HORIZONTAL,\
+length=300,width=20,label="Temperature",tickinterval=0.2,variable=temp)#,command=maj)
+widget_temp.pack(padx=10,pady=10)
 
 # Création d'un widget Button (bouton Quitter)
-BoutonQuitter = Button(Mafenetre, text ='Quitter', command = Mafenetre.destroy)
-BoutonQuitter.pack(side = LEFT, padx = 5, pady = 5)
+BoutonQuitter = Button(Mafenetre, text ='Quitter', command = Mafenetre.destroy,activebackground= "white",bg = "white")
+BoutonQuitter.pack(side = BOTTOM, padx = 15, pady = 20)
 Mafenetre.mainloop()
-
